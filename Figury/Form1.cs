7003191e2c3c6280ea380a9ddec635969ea610f3
wallker <twallker@wp.pl>
2025-02-15 20:06:02 +0100

    Jakiś problem przy dodawaniu koł i kwadratów, prostokąty działają!

--- Figury/Form1.cs
+++ Figury/Form1.cs
@@ -15,12 +15,7 @@ namespace Figury
             InitializeComponent();
             dostępneKontrolki = [Tuple.Create(p0,v0), Tuple.Create(p1, v1), Tuple.Create(p2, v2),
                                  Tuple.Create(p3, v3), Tuple.Create(p4, v4), Tuple.Create(p5, v5), Tuple.Create(p6, v6)];
-            //dostępneParametry = [p0, p1, p2, p3, p4, p5, p6];
             dostępneWartosci = [v0, v1, v2, v3, v4, v5, v6];
-            //if (dostępneParametry.Count != dostępneWartosci.Count)
-            //{
-            //    throw new InvalidOperationException("Musi być równa ilość opisów i wartości");
-            //}
             foreach (IFigury f in dostepneFigury)
             {
                 zasobnikFigur.Items.Add(f.nazwa);
@@ -28,34 +23,6 @@ namespace Figury
             zasobnikFigur.SelectedIndex = 0;
             figuries = new List<IFigury>();
         }
-
-        //private void AktualizujParametry_sks(ReadOnlyCollection<string> etykiety, List<decimal> wartości)
-        //{
-        //    if (etykiety.Count != wartości.Count)
-        //    {
-        //        throw new InvalidOperationException("Ilość parametrów i wartości defaultowych sie nie zgadza");
-        //    }
-        //    if (etykiety.Count > dostępneParametry.Count)
-        //    {
-        //        throw new InvalidOperationException("Brak miejsca na parametry");
-        //    }
-        //    for (int i = 0; i < dostępneParametry.Count; i++)
-        //    {
-        //        if (i < etykiety.Count)
-        //        {
-        //            dostępneParametry[i].Text = etykiety[i];
-        //            dostępneParametry[i].Visible = true;
-        //            dostępneWartosci[i].Visible = true;
-        //            dostępneWartosci[i].Value = wartości[i];
-        //        }
-        //        else
-        //        {
-        //            dostępneParametry[i].Text = "";
-        //            dostępneParametry[i].Visible = false;
-        //            dostępneWartosci[i].Visible = false;
-        //        }
-        //    }
-        //}
 
         private void AktualizujParametry2(Dictionary<string, decimal> parametry)
         {            
@@ -97,7 +64,6 @@ namespace Figury
             {
                 if (f.nazwa == zasobnikFigur.Text)
                 {
-                    //AktualizujParametry(f.GetParamNames, f.GetDefaultVal);
                     AktualizujParametry2(f.GetParams);
                     return;
                 }
@@ -113,50 +79,22 @@ namespace Figury
         {//dodaj nową figurę
             foreach (IFigury f in dostepneFigury)
             {
-                //if (f.nazwa == zasobnikFigur.Text)
-                //{
-
-                //    IFigury obj = (IFigury)Activator.CreateInstance(f.GetType());
-                //    if (obj == null) { throw new InvalidOperationException("Nie utworzono biektu!"); }
-                //    List<decimal> param = new List<decimal>();
-                //    foreach (System.Windows.Forms.NumericUpDown l in dostępneWartosci)
-                //    {
-                //        if (l.Visible) param.Add(l.Value);
-                //    }
-
-                //    if (obj.SetParameters(param))
-                //    {//parametry przyjęte
-                //        figuries.Add(obj);
-                //        string nazwa = obj.nazwa + "_" + obj.id;
-                //        userFig.Items.Add(nazwa);
-                //        userFig.Text = nazwa;
-                //        IFigury.licznik++;
-                //    }
-                //    Rysuj();
-                //}
                 if (f.nazwa == zasobnikFigur.Text)
                 {
 
                     IFigury obj = (IFigury)Activator.CreateInstance(f.GetType());
                     if (obj == null) { throw new InvalidOperationException("Nie utworzono biektu!"); }
 
-                    bool paramsOk = true;
+
+                    Dictionary<string, decimal> wartosci = new Dictionary<string, decimal>();
                     foreach (Tuple<System.Windows.Forms.Label, System.Windows.Forms.NumericUpDown> para in dostępneKontrolki)
                     {
-                        if (para.Item2.Visible)
+                        if (para.Item1.Visible && para.Item2.Visible)
                         {
-                            obj.SetParam(para.Item1.Text, para.Item2.Value);
+                            wartosci.Add(para.Item1.Text, para.Item2.Value);
                         }
                     }
-
-
-                    List<decimal> param = new List<decimal>();
-                    foreach (System.Windows.Forms.NumericUpDown l in dostępneWartosci)
-                    {
-                        if (l.Visible) param.Add(l.Value);
-                    }
-
-                    if (obj.SetParameters(param))
+                    if (obj.SetParameters(wartosci))
                     {//parametry przyjęte
                         figuries.Add(obj);
                         string nazwa = obj.nazwa + "_" + obj.id;
@@ -176,8 +114,14 @@ namespace Figury
             {
                 if (userFig.Text == f.nazwa + "_" + f.id)
                 {
-                    //AktualizujParametry(f.GetParamNames, f.m_parametry);
-                    AktualizujParametry2(f.GetParams);
+                    if (zasobnikFigur.Text == f.nazwa)
+                    {
+                        AktualizujParametry2(f.GetParams);//todo błąd!
+                    }
+                    else
+                    {
+                        zasobnikFigur.Text = f.nazwa;
+                    }
                     return;
                 }
             }
@@ -190,12 +134,16 @@ namespace Figury
             {
                 if (userFig.Text == f.nazwa + "_" + f.id)
                 {
-                    List<decimal> param = new List<decimal>();
-                    foreach (System.Windows.Forms.NumericUpDown l in dostępneWartosci)
+                    Dictionary<string, decimal> wartosci = new Dictionary<string, decimal>();
+                    foreach (Tuple<System.Windows.Forms.Label, System.Windows.Forms.NumericUpDown> para in dostępneKontrolki)
                     {
-                        if (l.Visible) param.Add(l.Value);
+                        if (para.Item1.Visible && para.Item2.Visible)
+                        {
+                            wartosci.Add(para.Item1.Text, para.Item2.Value);
+                        }
                     }
-                    f.SetParameters(param);
+                    f.SetParameters(wartosci);
+                    Rysuj();
                     return;
                 }
             }
@@ -208,8 +156,17 @@ namespace Figury
                 if (userFig.Text == f.nazwa + "_" + f.id)
                 {
                     userFig.Items.Remove(f.nazwa + "_" + f.id);
+                    int index = figuries.IndexOf(f);
                     figuries.Remove(f);
-                    zasobnikFigur_SelectedIndexChanged(sender, e);
+                    if (figuries.Count > index)
+                    {
+                        userFig.SelectedIndex = index;
+                    }
+                    else
+                    {
+                        userFig.SelectedIndex = figuries.Count-1;
+                    }
+                    Rysuj();
                     return;
                 }
             }
@@ -220,7 +177,6 @@ namespace Figury
         public IFigury()
         {
             nazwa = this.GetType().Name;
-            m_parametry = new List<decimal>();
             id = licznik;
         }
 
@@ -232,22 +188,40 @@ namespace Figury
 
         public abstract Dictionary<string, decimal> GetParams { get; }
 
-        public abstract bool SetParam(string key, decimal value);
-
-
-        public List<decimal> m_parametry { get; private set; }
-
         public abstract ReadOnlyCollection<string> GetParamNames { get; }
 
         public abstract List<decimal> GetDefaultVal { get; }
 
-        virtual public bool SetParameters(List<decimal> parametry)
+        virtual public bool SetParameters(Dictionary<string, decimal> parametry)
         {
-            if (parametry.Count != GetParamNames.Count)
-            { 
-                return false; 
+            foreach (var param in GetParams)
+            {
+                if (parametry.ContainsKey(param.Key))
+                {
+                    GetParams[param.Key] = parametry[param.Key];
+                }
+                else
+                {
+                    throw new Exception("Nie zgadzają się parametry");
+                    //return false;
+                }
             }
-            m_parametry = parametry;
+            return true;
+        }
+
+        virtual protected bool CheckParameters(Dictionary<string, decimal> parametry)
+        {
+            if (parametry.Count != GetParams.Count)
+            {
+                return false;
+            }
+            foreach (var param in GetParams)
+            {
+                if (!parametry.ContainsKey(param.Key))
+                {
+                    return false;
+                }
+            }
             return true;
         }
 
@@ -256,43 +230,38 @@ namespace Figury
 
     public class Prostokąt : IFigury
     {
-        public Dictionary<string, decimal> m_parametryf = new Dictionary<string, decimal> { { "Wys", 10 }, { "Szer", 20 }, { "X", 0 }, { "Y", 0 } };
+        private readonly Dictionary<string, decimal> m_parametryf = new Dictionary<string, decimal> { { "Wys", 10 }, { "Szer", 20 }, { "X", 0 }, { "Y", 0 } };        
         public override Dictionary<string, decimal> GetParams { get { return m_parametryf; } }
 
-        public override bool SetParam(string key, decimal value)
+        public override bool SetParameters(Dictionary<string, decimal> parametry)
         {
-            if (!m_parametryf.ContainsKey(key))
+            if (!CheckParameters(parametry))
             {
                 return false;
             }
-
-            m_parametryf["not"] = 5;
-            return false;
+            if ((parametry["Wys"]<=0) || (parametry["Szer"]<=0))
+            {
+                return false;
+            }
+            return base.SetParameters(parametry);
         }
 
         public override ReadOnlyCollection<string> GetParamNames { get { return new List<string> { "Wys", "Szer", "X", "Y" }.AsReadOnly(); } }
         public override List<decimal> GetDefaultVal { get { return new List<decimal> { 10, 20, 0, 0 }; } }
 
-        override public bool SetParameters(List<decimal> parametry)
-        {
-            if ((parametry[0] <= 0) || (parametry[1]) <= 0)
-            {
-                return false;
-            }            
-            return base.SetParameters(parametry);
-        }
-
         public override void Rysuj(Graphics g, int width, int height)
         {
-            float wysHalf = (float)m_parametry[0]/2;
-            float szerHalf = (float)m_parametry[1]/2;
+            float wysHalf = (float)m_parametryf["Wys"]/2;
+            float szerHalf = (float)m_parametryf["Szer"]/2;
+            float X = (float)m_parametryf["X"] + width/2;
+            float Y = -(float)m_parametryf["Y"] + height/2;
             PointF[] punkty = new PointF[4];
-            punkty[0] = new PointF(-szerHalf, wysHalf);
-            punkty[1] = new PointF(szerHalf, wysHalf);
-            punkty[2] = new PointF(szerHalf, -wysHalf);
-            punkty[3] = new PointF(-szerHalf, -wysHalf);            
-            //Random rnd = new Random();
-            //Point[] points={ new Point(0, 0), new Point(rnd.Next(10,30), rnd.Next(10,30)) };
+            punkty[0] = new PointF(-szerHalf+X, wysHalf+Y);
+            punkty[1] = new PointF(szerHalf+X, wysHalf+Y);
+            punkty[2] = new PointF(szerHalf+X, -wysHalf + Y);
+            punkty[3] = new PointF(-szerHalf+X, -wysHalf + Y);            
+            ////Random rnd = new Random();
+            ////Point[] points={ new Point(0, 0), new Point(rnd.Next(10,30), rnd.Next(10,30)) };
             Pen pen = new Pen(Color.DarkGreen);
             g.DrawPolygon(pen, punkty);
         }
@@ -300,58 +269,52 @@ namespace Figury
 
     public class Kwadrat : IFigury
     {
-        public Dictionary<string, decimal> m_parametryf = new Dictionary<string, decimal> { { "Bok", 20 }, { "X", 0 }, { "Y", 0 } };
+        private readonly Dictionary<string, decimal> m_parametryf = new Dictionary<string, decimal> { { "Bok", 20 }, { "X", 0 }, { "Y", 0 } };
         public override Dictionary<string, decimal> GetParams { get { return m_parametryf; } }
 
-        public override bool SetParam(string key, decimal value)
+        public override bool SetParameters(Dictionary<string, decimal> parametry)
         {
-            return false;
+            return true;
         }
 
 
         public override ReadOnlyCollection<string> GetParamNames { get { return new List<string> { "Bok", "X", "Y" }.AsReadOnly(); } }
         public override List<decimal> GetDefaultVal { get { return new List<decimal> { 10, 0, 0 }; } }
 
-        override public bool SetParameters(List<decimal> parametry)
-        {
-            if (parametry[0] <= 0)
-            {
-                return false;
-            }
-            return base.SetParameters(parametry);
-        }
         public override void Rysuj(Graphics g, int width, int height)
         {
-            throw new NotImplementedException();
+            float bokHalf = (float)m_parametryf["Bok"] / 2;
+            float X = (float)m_parametryf["X"] + width / 2;
+            float Y = -(float)m_parametryf["Y"] + height / 2;
+            PointF[] punkty = new PointF[4];
+            punkty[0] = new PointF(-bokHalf + X, bokHalf + Y);
+            punkty[1] = new PointF(bokHalf + X, bokHalf + Y);
+            punkty[2] = new PointF(bokHalf + X, -bokHalf + Y);
+            punkty[3] = new PointF(-bokHalf + X, -bokHalf + Y);
+            ////Random rnd = new Random();
+            ////Point[] points={ new Point(0, 0), new Point(rnd.Next(10,30), rnd.Next(10,30)) };
+            Pen pen = new Pen(Color.DarkGreen);
+            g.DrawPolygon(pen, punkty);
         }
     }
 
     public class Koło : IFigury
     {
-        public Dictionary<string, decimal> m_parametryf = new Dictionary<string, decimal> { { "R", 30 }, { "X", 0 }, { "Y", 0 } };
+        private readonly Dictionary<string, decimal> m_parametryf = new Dictionary<string, decimal> { { "R", 30 }, { "X", 0 }, { "Y", 0 } };
         public override Dictionary<string, decimal> GetParams { get { return m_parametryf; } }
-
-        public override bool SetParam(string key, decimal value)
-        {
-            return false;
-        }
-
-
 
         public override ReadOnlyCollection<string> GetParamNames { get { return new List<string> { "R", "X", "Y" }.AsReadOnly(); } }
         public override List<decimal> GetDefaultVal { get { return new List<decimal> { 20, 0, 0 }; } }
 
-        override public bool SetParameters(List<decimal> parametry)
-        {
-            if (parametry[0] <= 0)
-            {
-                return false;
-            }
-            return base.SetParameters(parametry);
-        }
         public override void Rysuj(Graphics g, int width, int height)
         {
-            throw new NotImplementedException();
+            float R = (float)m_parametryf["R"];
+            float X = (float)m_parametryf["X"] + width / 2 - R / 2;
+            float Y = -(float)m_parametryf["Y"] + height / 2 - R / 2;            
+            ////Random rnd = new Random();
+            ////Point[] points={ new Point(0, 0), new Point(rnd.Next(10,30), rnd.Next(10,30)) };
+            Pen pen = new Pen(Color.DarkGreen);
+            g.DrawEllipse(pen, X, Y, R, R);
         }
     }
 }
